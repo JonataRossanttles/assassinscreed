4733c711a96d1ccb4125b48a3ad8ecea185e8764
Jonata Rossanttles <105800872+JonataRossanttles@users.noreply.github.com>
2024-07-19 08:54:51 -0300

    Add files via upload

--- src/datacontext.js
+++ src/datacontext.js
@@ -1,20 +1,25 @@
 import React, { createContext, useState, useEffect } from 'react';
 
 
-
-
 const DataContext = createContext();
 
 export const Provider = ({ children }) => {
   const [information, setInformation] = useState([]);
   const localData = localStorage.getItem('assassinsCreedData');
-  const [localDatajson,SetlocalDatajson] = useState()  ;
+  const [localDatajson,setlocalDatajson] = useState()  ;
  document.body.style.backgroundColor = 'black'
 
 useEffect(()=>{
   if(localData){
-    setInformation(JSON.parse(localData))
-    SetlocalDatajson( JSON.parse(localData)) 
+      localStorage.clear()
+      fetch('/assassinsCreed.json')
+      .then((response) => response.json())
+      .then(
+        (data) => {setInformation(data)
+         localStorage.setItem('assassinsCreedData',JSON.stringify(data)) 
+         
+         setlocalDatajson( JSON.parse(localData))             
+                  });
    }else{
     
       fetch('/assassinsCreed.json')
@@ -24,7 +29,6 @@ useEffect(()=>{
            localStorage.setItem('assassinsCreedData',JSON.stringify(data)) 
                         
                     });
-    
    }
 
 },[])
