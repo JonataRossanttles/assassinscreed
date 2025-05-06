3ca5dfdcaea66b61adcb5b61f501d32bce3376c3
Jonata Rossanttles <105800872+JonataRossanttles@users.noreply.github.com>
2025-05-06 11:13:47 -0300

    Update section.js

--- src/components/section.js
+++ src/components/section.js
@@ -28,7 +28,7 @@ document.body.style.marginTop = "0"
  },[parametro])
 
 useEffect(()=>{
-   window.scrollTo(0, 0)
+   
   if(localstorage){
     const obj =  localstorage.find(element => element.name === parametro.id)
    
